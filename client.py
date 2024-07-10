b7c3c76d20485ae13a5f4f0ad2eb8c56ab540325
liadav <liadav6@gmail.com>
2024-07-10 22:49:55 +0300

    remove unsed

--- client.py
+++ client.py
@@ -44,8 +44,6 @@ class QVAPDevice:
         self.characteristic = None
         self.serial_characteristic = None
         self.application_firmware = 0
-        self.ble_process_pending = False
-        self.timer_ble_pending_handler = None
         
         # Parameters to store
         self.current_temp = None
@@ -58,23 +56,6 @@ class QVAPDevice:
         self.charger_status = None
         self.settings_flags = None
 
-    def set_ble_process_pending(self, status):
-        self.ble_process_pending = status
-        if status:
-            if self.timer_ble_pending_handler is None:
-                self.timer_ble_pending_handler = asyncio.get_event_loop().call_later(2, self.my_timer_ble_pending_timeout_func)
-        else:
-            if self.timer_ble_pending_handler:
-                self.timer_ble_pending_handler.cancel()
-                self.timer_ble_pending_handler = None
-
-    def is_ble_process_pending(self):
-        return self.ble_process_pending
-
-    def my_timer_ble_pending_timeout_func(self):
-        print("BLE operation timed out")
-        self.set_ble_process_pending(False)
-
     async def connect(self):
         async with BleakClient(self.address) as client:
             self.client = client
@@ -83,28 +64,22 @@ class QVAPDevice:
             await self.enable_notifications()
 
             await self.init_connection()
-            await asyncio.sleep(2)
+            await asyncio.sleep(0.5)
 
-            # Send command to initialize and retrieve firmware status
             await self.setup_initial_params()
-            await asyncio.sleep(2)
+            await asyncio.sleep(0.5)
             await self.prepare_device()
-            await asyncio.sleep(2)
+            await asyncio.sleep(0.5)
             await self.additional_setup()
-            await asyncio.sleep(2)
+            await asyncio.sleep(0.5)
             await self.final_setup()
-            await asyncio.sleep(2)
-
-
-            #if self.application_firmware == 0:
-            #    print("Failed to set application firmware. Exiting.")
-            #    return
+            await asyncio.sleep(0.5)
 
             if self.serial_service:
                 await self.read_serial_number()
 
             await self.final_stage()
-            await asyncio.sleep(2)
+            await asyncio.sleep(0.5)
             await self.start_periodic_updates()
 
     async def discover_services(self):
@@ -120,34 +95,19 @@ class QVAPDevice:
         self.notifications_enabled = True
 
     async def init_connection(self):
-        if not self.is_ble_process_pending():
-            self.set_ble_process_pending(True)
-            await self.send_command(COMMANDS["INIT_CONNECTION"])
-            self.set_ble_process_pending(False)
+        await self.send_command(COMMANDS["INIT_CONNECTION"])
 
     async def setup_initial_params(self):
-        if not self.is_ble_process_pending():
-            self.set_ble_process_pending(True)
-            await self.send_command(COMMANDS["SETUP_INITIAL_PARAMS"])
-            self.set_ble_process_pending(False)
+        await self.send_command(COMMANDS["SETUP_INITIAL_PARAMS"])
 
     async def prepare_device(self):
-        if not self.is_ble_process_pending():
-            self.set_ble_process_pending(True)
-            await self.send_command(COMMANDS["PREPARE_DEVICE"])
-            self.set_ble_process_pending(False)
+        await self.send_command(COMMANDS["PREPARE_DEVICE"])
 
     async def additional_setup(self):
-        if not self.is_ble_process_pending():
-            self.set_ble_process_pending(True)
-            await self.send_command(COMMANDS["ADDITIONAL_SETUP"])
-            self.set_ble_process_pending(False)
+        await self.send_command(COMMANDS["ADDITIONAL_SETUP"])
 
     async def final_setup(self):
-        if not self.is_ble_process_pending():
-            self.set_ble_process_pending(True)
-            await self.send_command(COMMANDS["FINAL_SETUP"])
-            self.set_ble_process_pending(False)
+        await self.send_command(COMMANDS["FINAL_SETUP"])
 
     async def read_serial_number(self):
         if "read" in self.serial_characteristic.properties:
@@ -159,7 +119,6 @@ class QVAPDevice:
     async def final_stage(self):
         buffer = bytearray(20)
         buffer[1] = 6
-        print(f"application_firmware={self.application_firmware}")
         if self.application_firmware & APPLICATION_FIRMWARE_MASK_APPLICATION:
             print("Firmware application valid. Starting periodic updates.")
             buffer[0] = 0x30  # BOOTLOADER_UPDATE
@@ -175,13 +134,8 @@ class QVAPDevice:
             await asyncio.sleep(4)  # Periodic interval
 
     async def send_command(self, command, data=[]):
-        if not self.is_ble_process_pending():
-            self.set_ble_process_pending(True)
-            try:
-                buffer = bytearray([command] + data + [0] * (20 - len(data) - 1))
-                await self.client.write_gatt_char(self.characteristic, buffer)
-            finally:
-                self.set_ble_process_pending(False)
+        buffer = bytearray([command] + data + [0] * (20 - len(data) - 1))
+        await self.client.write_gatt_char(self.characteristic, buffer)
 
     async def send_periodic_command(self):
         await self.send_command(COMMANDS["PREPARE_DEVICE"])
@@ -192,120 +146,116 @@ class QVAPDevice:
 
     async def parse_response(self, response):
         command = response[0]
-        try:
-            if command == 2:  # Command that updates the application firmware status
-                self.application_firmware = response[1]
-                firmware_version = response[2:8].decode("utf-8")
-                bootloader_version = response[11:17].decode("utf-8")
-                print(f"Application Firmware: {firmware_version}, Bootloader: {bootloader_version}")
-                # Handle further firmware version comparison and update
-            elif command in {1, 48}:
-                self.application_firmware = APPLICATION_FIRMWARE_MASK_APPLICATION
-                if self.application_firmware & APPLICATION_FIRMWARE_MASK_APPLICATION and command != 48:
-                    self.current_temp = (response[3] << 8) + response[2]
-                    self.set_temp = (response[5] << 8) + response[4]
-                    self.boost_temp = response[6]
-                    self.superboost_temp = response[7]
-                    self.battery_level = response[8]
-                    self.auto_shutoff = (response[10] << 8) + response[9]
-                    self.heater_mode = response[11]
-                    self.charger_status = response[13]
-                    self.settings_flags = response[14]
+        if command == 2:  # Command that updates the application firmware status
+            self.application_firmware = response[1]
+            firmware_version = response[2:8].decode("utf-8")
+            bootloader_version = response[11:17].decode("utf-8")
+            print(f"Application Firmware: {firmware_version}, Bootloader: {bootloader_version}")
+            # Handle further firmware version comparison and update
+        elif command in {1, 48}:
+            if self.application_firmware & APPLICATION_FIRMWARE_MASK_APPLICATION and command != 48:
+                self.current_temp = (response[3] << 8) + response[2]
+                self.set_temp = (response[5] << 8) + response[4]
+                self.boost_temp = response[6]
+                self.superboost_temp = response[7]
+                self.battery_level = response[8]
+                self.auto_shutoff = (response[10] << 8) + response[9]
+                self.heater_mode = response[11]
+                self.charger_status = response[13]
+                self.settings_flags = response[14]
 
-                    print(f"Current Temp: {self.current_temp}, Set Temp: {self.set_temp}, Boost Temp: {self.boost_temp}, Superboost Temp: {self.superboost_temp}, Battery: {self.battery_level}, Auto-shutoff: {self.auto_shutoff}, Heater Mode: {self.heater_mode}, Charger: {self.charger_status}, Settings: {self.settings_flags}")
+                print(f"Current Temp: {self.current_temp}, Set Temp: {self.set_temp}, Boost Temp: {self.boost_temp}, Superboost Temp: {self.superboost_temp}, Battery: {self.battery_level}, Auto-shutoff: {self.auto_shutoff}, Heater Mode: {self.heater_mode}, Charger: {self.charger_status}, Settings: {self.settings_flags}")
 
-                else:
-                    print("_______cmd0x01 - bootloader")
-                    # Handle bootloader update steps
-                    status = response[1]
-                    if status == 1:
-                        self.data_idx_qvap += 1
-                        if self.data_idx_qvap >= self.page_size_qvap / self.data_size_firmware_per_packet:
-                            print("page write start request")
-                        else:
-                            print("page data write request")
-                        await self.write_page_data_sequence_qvap()
-                    elif status == 2:
-                        self.page_idx_qvap += 1
-                        self.data_idx_qvap = 0
-                        print(f"page {self.page_idx_qvap} write done")
-                        if self.page_idx_qvap < self.binary_number_of_pages:
-                            await self.write_page_data_sequence_qvap()
-                        else:
-                            await asyncio.sleep(0.2)
-                            await self.start_qvap_application(True)
-                    elif status == 3:
-                        pass
-                    elif status == 4:
-                        pass
-                    elif status == 5:
-                        pass
-                    elif status == 6:
-                        connection_interval = response[2]
-                        print(f"   connection interval {connection_interval}")
-                        if connection_interval < 25:
-                            print("BLE connection interval too small; Firmware upgrade will fail")
-                    elif status == 8:
-                        print("  decrypt data done")
-                        buffer = bytearray(3)
-                        buffer[0] = 0x30 if self.update_bootloader else 0x01
-                        buffer[1] = 2
-                        buffer[2] = self.page_idx_qvap
-                        self.timeout_ble_response_qvap = asyncio.create_task(self.handle_timeout_ble_response_missing_qvap(0.75))
-                        await self.client.write_gatt_char(self.characteristic, buffer)
-                    elif status == 34:
-                        print("Erase failed: please reload and retry.\n")
-                    elif status == 19:
-                        print("Validation failed: please reload and retry.\n")
-                        if self.nb_retries_validation < 1:
-                            await self.start_qvap_application(True)
-                            self.nb_retries_validation += 1
-                    elif status == 51:
-                        print("Validation failed (mode): please reload and retry.\n")
-                    elif status == 35:
-                        print("Validation failed: please reload and retry.\n")
-                    elif status == 82:
-                        print("VersionMajor failed: please reload and retry.\n")
-                    elif status == 98:
-                        print("VersionMinor failed: please reload and retry.\n")
-                    else:
-                        print(f"   undefined status {status} - {response[2]}")
-            elif command == 3:
-                err_code = response[1]
-                err_category = response[2]
-                if err_category == 4:
-                    print("Start analysis")
-                else:
-                    print("Device analysis successful")
-                    if response[3] < 9:
-                        print("Display brightness reduced")
-                    if response[4] & 1:
-                        print("Charge limit activated")
-                    if not response[5] & 1:
-                        print("Boost visualization disabled")
-                    if response[6] & 1:
-                        print("Charge optimization activated")
-                    if not response[7] & 1:
-                        print("Vibration disabled")
-            elif command == 4:
-                if len(response) >= 20:
-                    heater_runtime_minutes = int.from_bytes(response[1:4], byteorder='little')
-                    battery_charging_time_minutes = int.from_bytes(response[4:7], byteorder='little')
-                    print(f"Heater Runtime: {heater_runtime_minutes} minutes, Battery Charging Time: {battery_charging_time_minutes} minutes")
-            elif command == 5:
-                if len(response) >= 17:
-                    device_prefix = response[15:17].decode("utf-8")
-                    device_name = response[9:15].decode("utf-8")
-                    print(f"Device Prefix: {device_prefix}, Device Name: {device_name}")
-            elif command == 6:
-                if len(response) >= 5:
-                    brightness = response[2]
-                    vibration = response[5]
-                    print(f"Brightness: {brightness}, Vibration: {'Enabled' if vibration else 'Disabled'}")
             else:
-                print(f"Default command {command}")
-        finally:
-            self.set_ble_process_pending(False)
+                print("_______cmd0x01 - bootloader")
+                # Handle bootloader update steps
+                status = response[1]
+                if status == 1:
+                    self.data_idx_qvap += 1
+                    if self.data_idx_qvap >= self.page_size_qvap / self.data_size_firmware_per_packet:
+                        print("page write start request")
+                    else:
+                        print("page data write request")
+                    await self.write_page_data_sequence_qvap()
+                elif status == 2:
+                    self.page_idx_qvap += 1
+                    self.data_idx_qvap = 0
+                    print(f"page {self.page_idx_qvap} write done")
+                    if self.page_idx_qvap < self.binary_number_of_pages:
+                        await self.write_page_data_sequence_qvap()
+                    else:
+                        await asyncio.sleep(0.2)
+                        await self.start_qvap_application(True)
+                elif status == 3:
+                    pass
+                elif status == 4:
+                    pass
+                elif status == 5:
+                    pass
+                elif status == 6:
+                    connection_interval = response[2]
+                    print(f"   connection interval {connection_interval}")
+                    if connection_interval < 25:
+                        print("BLE connection interval too small; Firmware upgrade will fail")
+                elif status == 8:
+                    print("  decrypt data done")
+                    buffer = bytearray(3)
+                    buffer[0] = 0x30 if self.update_bootloader else 0x01
+                    buffer[1] = 2
+                    buffer[2] = self.page_idx_qvap
+                    self.timeout_ble_response_qvap = asyncio.create_task(self.handle_timeout_ble_response_missing_qvap(0.75))
+                    await self.client.write_gatt_char(self.characteristic, buffer)
+                elif status == 34:
+                    print("Erase failed: please reload and retry.\n")
+                elif status == 19:
+                    print("Validation failed: please reload and retry.\n")
+                    if self.nb_retries_validation < 1:
+                        await self.start_qvap_application(True)
+                        self.nb_retries_validation += 1
+                elif status == 51:
+                    print("Validation failed (mode): please reload and retry.\n")
+                elif status == 35:
+                    print("Validation failed: please reload and retry.\n")
+                elif status == 82:
+                    print("VersionMajor failed: please reload and retry.\n")
+                elif status == 98:
+                    print("VersionMinor failed: please reload and retry.\n")
+                else:
+                    print(f"   undefined status {status} - {response[2]}")
+        elif command == 3:
+            err_code = response[1]
+            err_category = response[2]
+            if err_category == 4:
+                print("Start analysis")
+            else:
+                print("Device analysis successful")
+                if response[3] < 9:
+                    print("Display brightness reduced")
+                if response[4] & 1:
+                    print("Charge limit activated")
+                if not response[5] & 1:
+                    print("Boost visualization disabled")
+                if response[6] & 1:
+                    print("Charge optimization activated")
+                if not response[7] & 1:
+                    print("Vibration disabled")
+        elif command == 4:
+            if len(response) >= 20:
+                heater_runtime_minutes = int.from_bytes(response[1:4], byteorder='little')
+                battery_charging_time_minutes = int.from_bytes(response[4:7], byteorder='little')
+                print(f"Heater Runtime: {heater_runtime_minutes} minutes, Battery Charging Time: {battery_charging_time_minutes} minutes")
+        elif command == 5:
+            if len(response) >= 17:
+                device_prefix = response[15:17].decode("utf-8")
+                device_name = response[9:15].decode("utf-8")
+                print(f"Device Prefix: {device_prefix}, Device Name: {device_name}")
+        elif command == 6:
+            if len(response) >= 5:
+                brightness = response[2]
+                vibration = response[5]
+                print(f"Brightness: {brightness}, Vibration: {'Enabled' if vibration else 'Disabled'}")
+        else:
+            print(f"Default command {command}")
 
     async def write_page_data_sequence_qvap(self):
         # Implement the write page data sequence logic here
