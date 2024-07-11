b188dc5461c9bce44cf78f83c893000d667b12d3
liadav <liadav6@gmail.com>
2024-07-11 09:10:28 +0300

    structure

--- components/qvap/sensor.py
+++ components/qvap/sensor.py
@@ -5,10 +5,10 @@ from esphome.const import UNIT_EMPTY, ICON_EMPTY
 
 qvap_ns = cg.esphome_ns.namespace("qvap")
 
-qvap = qvap_ns.class_("qvap", sensor.Sensor, cg.PollingComponent)
+QVap = qvap_ns.class_("QVap", sensor.Sensor, cg.PollingComponent)
 
 CONFIG_SCHEMA = sensor.sensor_schema(
-    qvap, unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=1
+    QVap, unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=1
 ).extend(cv.polling_component_schema("60s"))
 
 
