ee1ae390854a9e78c231f78a915a7574c7c95d55
liadav <liadav6@gmail.com>
2024-07-13 20:47:42 +0300

    fixes

--- components/qvap/sensor.py
+++ components/qvap/sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, esp32_ble_client
+from esphome.components import sensor, ble_client
 from esphome.const import (
     CONF_MAC_ADDRESS,
     CONF_ID,
@@ -8,7 +8,7 @@ from esphome.const import (
 
 qvap_ns = cg.esphome_ns.namespace("qvap")
 
-QVap = qvap_ns.class_("QVap", sensor.Sensor, cg.PollingComponent)
+QVap = qvap_ns.class_("QVap", esp32_client.BLEClient, cg.Component)
 
 #CONFIG_SCHEMA = sensor.sensor_schema(
 #    QVap, unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=1
@@ -18,21 +18,16 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(QVap),
-            cv.Required(CONF_ID): cv.esp32_ble_client,
-#            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-#                unit_of_measurement=UNIT_CELSIUS,
-#                accuracy_decimals=2,
-#                device_class=DEVICE_CLASS_TEMPERATURE,
-#                state_class=STATE_CLASS_MEASUREMENT,
-#            ),
+            cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
         }
     )
-    .extend(cv.polling_component_schema("4s"))
+    .extend(ble_client.BLE_CLIENT_SCHEMA)
     .extend(cv.COMPONENT_SCHEMA)
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await ble_client.register_ble_device(var, config)
 
     cg.add(var.set_address(config[CONF_MAC_ADDRESS].as_hex))
