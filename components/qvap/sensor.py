57677fc16fd8f9e4d765ad01c7074f222b1c88e4
liadav <liadav6@gmail.com>
2024-07-13 20:30:11 +0300

    fixes

--- components/qvap/sensor.py
+++ components/qvap/sensor.py
@@ -1,7 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import UNIT_EMPTY, ICON_EMPTY
+from esphome.const import (
+    CONF_MAC_ADDRESS,
+    CONF_ID,
+)
 
 qvap_ns = cg.esphome_ns.namespace("qvap")
 
@@ -9,9 +12,27 @@ QVap = qvap_ns.class_("QVap", sensor.Sensor, cg.PollingComponent)
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     QVap, unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=1
-).extend(cv.polling_component_schema("4s"))
+)
 
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(RuuviTag),
+            cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
+#            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+#                unit_of_measurement=UNIT_CELSIUS,
+#                accuracy_decimals=2,
+#                device_class=DEVICE_CLASS_TEMPERATURE,
+#                state_class=STATE_CLASS_MEASUREMENT,
+#            ),
+        }
+    )
+    .extend(cv.polling_component_schema("4s"))
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
-    var = await sensor.new_sensor(config)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+
+    cg.add(var.set_address(config[CONF_MAC_ADDRESS].as_hex))
