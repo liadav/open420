a3449f789831e6e9ce9c451c4f4b61353fffe1a5
liadav <liadav6@gmail.com>
2024-07-13 20:50:52 +0300

    fixes

--- components/qvap/sensor.py
+++ components/qvap/sensor.py
@@ -8,7 +8,7 @@ from esphome.const import (
 
 qvap_ns = cg.esphome_ns.namespace("qvap")
 
-QVap = qvap_ns.class_("QVap", ble_client.BLEClient, cg.Component)
+QVap = qvap_ns.class_("QVap", ble_client.BLEClientNode, cg.Component)
 
 #CONFIG_SCHEMA = sensor.sensor_schema(
 #    QVap, unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=1
@@ -18,7 +18,6 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(QVap),
-            cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
         }
     )
     .extend(ble_client.BLE_CLIENT_SCHEMA)
@@ -28,6 +27,5 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await ble_client.register_ble_device(var, config)
+    await ble_client.register_ble_node(var, config)
 
-    cg.add(var.set_address(config[CONF_MAC_ADDRESS].as_hex))
