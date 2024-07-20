3785dc34c8d5c73ea9ea402dd2faab5d16fcfa29
liadav <liadav6@gmail.com>
2024-07-20 20:54:23 +0300

    gpt fix

--- components/qvap_device/__init__.py
+++ components/qvap_device/__init__.py
@@ -1,22 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import esp32_ble_client
+from esphome.components import esp32_ble_tracker  # Import the correct module
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ['esp32_ble_client']
+DEPENDENCIES = ['esp32_ble_tracker']  # Adjust this based on actual dependencies
 
 qvap_ns = cg.esphome_ns.namespace('qvap_device')
-QVAPDevice = qvap_ns.class_('QVAPDevice', cg.Component, esp32_ble_client.ESP32BLEClient)
+QVAPDevice = qvap_ns.class_('QVAPDevice', cg.Component, esp32_ble_tracker.ESP32BLETracker)  # Use the correct class from esp32_ble_tracker
 
 CONF_IBEACON_UUID = 'ibeacon_uuid'
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(QVAPDevice),
     cv.Required(CONF_IBEACON_UUID): cv.hex_uint8_list,
-}).extend(cv.COMPONENT_SCHEMA).extend(esp32_ble_client.ESP32_BLE_CLIENT_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA).extend(esp32_ble_tracker.ESP32_BLE_TRACKER_SCHEMA)  # Adjust this based on the actual schema
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     cg.add(var.set_ibeacon_uuid(config[CONF_IBEACON_UUID]))
     yield cg.register_component(var, config)
-    yield esp32_ble_client.register_esp32_ble_client(var, config)
+    yield esp32_ble_tracker.register_esp32_ble_tracker(var, config)  # Use the correct registration function
